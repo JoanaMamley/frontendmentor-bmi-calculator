67713349217b354c29865438ab16415ec2989f86
JoanaMamley <joanateye18@gmail.com>
2024-02-28 15:21:57 +0000

    create grid design for large screen sizes

--- bmi-calculator/src/app/limitations/limitations.component.ts
+++ bmi-calculator/src/app/limitations/limitations.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-limitations',
   templateUrl: './limitations.component.html',
   styleUrls: ['./limitations.component.scss']
 })
-export class LimitationsComponent {
+export class LimitationsComponent implements OnInit{
+  hasLargeScreen!: boolean;
 
+  ngOnInit(): void {
+    this.hasLargeScreen = window.innerWidth >= 1440;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: unknown) {
+    this.hasLargeScreen = window.innerWidth >= 1440;
+  }
 }
